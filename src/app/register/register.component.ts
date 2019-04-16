f42049621a04125dffae085513f4458b716d56a0
ChelohHolmes <marcelogerstein@gmail.com>
2019-04-16 14:49:02 -0500

    Fix register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +10,25 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
+  UserNew: FormGroup;
   ngOnInit() {
+    this.UserNew = new FormGroup({
+      Name: new FormControl(),
+      LastName: new FormControl(),
+      Email: new FormControl(),
+      Gender: new FormControl(),
+      Birthday: new FormControl(),
+      User: new FormControl(),
+      NativeLanguage: new FormControl(),
+      Password: new FormControl(),
+      ConfirmPassword: new FormControl(),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.UserNew.value);
+    const form = JSON.stringify(this.UserNew.value);
+    console.log(form);
   }
 
 }

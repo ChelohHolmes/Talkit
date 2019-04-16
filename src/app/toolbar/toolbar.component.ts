12802ec6cfaef7681e975a498c2cb0eaeceb2327
ChelohHolmes <marcelogerstein@gmail.com>
2019-04-16 14:49:38 -0500

    Fix header

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
+
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    this.dialog.open(LoginComponent);
+  }
+
+  onRegister() {
+    this.dialog.open(RegisterComponent);
   }
 
 }

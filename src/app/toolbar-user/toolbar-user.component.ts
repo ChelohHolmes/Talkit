1f1ebb0a192311eabb4bb07f9c45b66329bef4a1
ChelohHolmes <marcelogerstein@gmail.com>
2019-04-19 01:25:29 -0500

    Add toolbar-user component and styling for new header

---
+++ src/app/toolbar-user/toolbar-user.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-user',
+  templateUrl: './toolbar-user.component.html',
+  styleUrls: ['./toolbar-user.component.scss']
+})
+export class ToolbarUserComponent implements OnInit {
+  user: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onUser() {
+  }
+}

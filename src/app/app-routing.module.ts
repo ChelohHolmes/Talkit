182460e70ff18d0647d5b17ace0b75824f5fbb34
ChelohHolmes <marcelogerstein@gmail.com>
2019-04-19 01:08:47 -0500

    Add new routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RecoveryComponent } from './recovery/recovery.component';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
+import { TranslatorComponent } from './translator/translator.component';
 
 const routes: Routes = [
-  { path: '', component: IndexComponent},
-  { path: 'Login', component:  LoginComponent},
-  { path: 'Register', component: RegisterComponent}
+  { path: '', component: IndexComponent },
+  { path: 'Login', component:  LoginComponent },
+  { path: 'Register', component: RegisterComponent },
+  { path: 'Recovery', component: RecoveryComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Friends', component: FriendsComponent },
+  { path: 'Translator', component: TranslatorComponent }
 ];
 
 @NgModule({

f53eacfdaa06fd2eadf132d3db4c93536e6581ad
ChelohHolmes <marcelogerstein@gmail.com>
2019-04-21 20:43:11 -0500

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,13 @@ import { RecoveryComponent } from './recovery/recovery.component';
 import { HomeComponent } from './home/home.component';
 import { FriendsComponent } from './friends/friends.component';
 import { TranslatorComponent } from './translator/translator.component';
+import { RecoverPassComponent } from './recover-pass/recover-pass.component';
+import { HolaComponent } from './hola/hola.component';
+import { CookiesComponent } from './cookies/cookies.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
+import { PrivacyPoliciesComponent } from './privacy-policies/privacy-policies.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { UpdateInfoComponent } from './update-info/update-info.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
@@ -15,7 +22,14 @@ const routes: Routes = [
   { path: 'Recovery', component: RecoveryComponent },
   { path: 'Home', component: HomeComponent },
   { path: 'Friends', component: FriendsComponent },
-  { path: 'Translator', component: TranslatorComponent }
+  { path: 'Translator', component: TranslatorComponent },
+  { path: 'Password_reset', component: RecoverPassComponent },
+  { path: 'Cookies_info', component: CookiesComponent },
+  { path: 'Privacy_policy', component: PrivacyPoliciesComponent },
+  { path: 'Terms_of_use', component: TermsOfUseComponent },
+  { path: 'Test', component: HolaComponent },
+  { path: 'Configuration', component: ConfigurationComponent },
+  { path: 'Update_info', component: UpdateInfoComponent}
 ];
 
 @NgModule({

cbba0dc6aab45a4b8b14c09a770931eecacf622a
ChelohHolmes <marcelogerstein@gmail.com>
2019-04-21 20:06:52 -0500

    Update random component

--- src/app/random/random.component.ts
+++ src/app/random/random.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {language} from '../register/register.component';
 
 @Component({
   selector: 'app-random',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RandomComponent implements OnInit {
 
+  RandomQueue: FormGroup;
+  languages: language[] = [
+    {value: 'Español', viewValue: 'Español'},
+    {value: 'Inglés', viewValue: 'English'},
+    {value: 'Francés', viewValue: 'Français'},
+    {value: 'Alemán', viewValue: 'Deutsch'},
+    {value: 'Italiano', viewValue: 'Italiano'},
+    {value: 'Portugués', viewValue: 'Português'},
+    {value: 'Ruso', viewValue: 'русский'},
+    {value: 'Chino', viewValue: '中国'},
+    {value: 'Japonés', viewValue: '日本語'},
+    {value: 'Coreano', viewValue: '한국어'}
+  ];
   constructor() { }
 
   ngOnInit() {
+    this.RandomQueue = new FormGroup({
+      Type: new FormControl(),
+      Language: new FormControl(),
+      Rol: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log(this.RandomQueue.value);
+    const form = JSON.stringify(this.RandomQueue.value);
+    console.log(form);
   }
 
 }

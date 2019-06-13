9624a4afe8cac9e6833c6af1b8febd8851b35e7f
ChelohHolmes <marcelogerstein@gmail.com>
2019-06-12 21:03:12 -0500

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {AfterContentChecked, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentChecked {
   title = 'Talkit';
-  user = false;
+  public user = false;
+
+  OnInit() {
+    this.onChanges();
+  }
+
+  ngAfterContentChecked() {
+    this.checkUser();
+  }
+
+  checkUser(): void {
+    try {
+      const user = sessionStorage.getItem('user');
+      this.user = user != null;
+    } catch (e) {
+      console.log('Error');
+    }
+
+  }
+
+  private onChanges() {
+    this.checkUser();
+  }
 }

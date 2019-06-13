938fa519f13d015e2146f651f909b12f7d0ea95f
ChelohHolmes <marcelogerstein@gmail.com>
2019-06-12 20:53:11 -0500

    Update recover-pass component

--- src/app/recover-pass/recover-pass.component.ts
+++ src/app/recover-pass/recover-pass.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {RecoverService} from '../services/recover.service';
+import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-recover-pass',
@@ -7,13 +10,16 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./recover-pass.component.scss']
 })
 export class RecoverPassComponent implements OnInit {
+  private match: boolean;
+  private sent: any;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private http: RecoverService, private snackBar: MatSnackBar, private router: Router) { }
   UserNew: FormGroup;
   password: string;
-  confirmpassword: string;
+  confirmPassword: string;
   hide = true;
   hide1 = true;
+  private email: string;
 
   ngOnInit() {
 
@@ -23,29 +29,41 @@ export class RecoverPassComponent implements OnInit {
         Validators.minLength(8),
         Validators.maxLength(128),
       ]],
-      ConfirmPassword: [this.confirmpassword, [
+      ConfirmPassword: [this.confirmPassword, [
         Validators.required
       ]]
     });
   }
 
   onSubmit() {
-    console.log(this.UserNew.value);
+    this.UserNew.value.Email = this.email;
     const form = JSON.stringify(this.UserNew.value);
+    // console.log(this.UserNew.value);
+    this.match = this.passwordsMatch(this.UserNew);
     console.log(form);
-    console.log(this.passwordsMatch(this.UserNew));
+    if (this.match) {
+      this.email = localStorage.getItem('Email');
+      this.http.posts(form).subscribe(data => {
+        this.sent = data;
+        console.log(this.sent);
+        if (this.sent === 1) {
+          this.snackBar.open('Contraseña cambiada.', 'OK', {
+          });
+          localStorage.removeItem('Email');
+        } else {
+          this.snackBar.open('Error.', 'OK', {
+          });
+        }
+      });
+    }
+
   }
 
   private passwordsMatch = (formp: FormGroup): boolean => {
-    if (formp.controls['Password'].touched && formp.controls['ConfirmPassword'].touched) {
-      if (formp.value.Password === formp.value.ConfirmPassword) {
-        return true;
-      } else {
-        return false;
-      }
+    if (formp.controls.Password.touched && formp.controls.ConfirmPassword.touched) {
+      return formp.value.Password === formp.value.ConfirmPassword;
     }
     return true;
   }
-
 
 }

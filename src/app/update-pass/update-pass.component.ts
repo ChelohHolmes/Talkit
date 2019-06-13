07646a4f814d3ba74753e795ff03bf75e55ed542
ChelohHolmes <marcelogerstein@gmail.com>
2019-06-12 21:01:49 -0500

    Update update-pass component

--- src/app/update-pass/update-pass.component.ts
+++ src/app/update-pass/update-pass.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-update-pass',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatePassComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  hide1 = true;
+  hide2 = true;
+  newpassword: string;
+  repeatpassword: string;
+  oldpassword: string;
+  UpdatePassword: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.UpdatePassword = this.formBuilder.group({
+      NewPassword: [this.newpassword, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(128),
+      ]],
+      RepeatPassword: [this.repeatpassword, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(128),
+      ]],
+      OldPassword: [this.oldpassword, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(128),
+      ]]
+    });
   }
 
+  onSubmit() {
+    console.log(this.UpdatePassword.value);
+    const form = JSON.stringify(this.UpdatePassword.value);
+    console.log(form);
+  }
+  private passwordsMatch = (formP: FormGroup): boolean => {
+    if (formP.controls.NewPassword.touched && formP.controls.RepeatPassword.touched) {
+      return formP.value.NewPassword === formP.value.RepeatPassword;
+    }
+    return true;
+  }
 }

a742857ea6ae05f65fd1b84c7e451a47b4196183
ChelohHolmes <marcelogerstein@gmail.com>
2019-06-12 20:56:39 -0500

    Update toolbar-user component

--- src/app/toolbar-user/toolbar-user.component.ts
+++ src/app/toolbar-user/toolbar-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PointsService} from '../services/points.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-toolbar-user',
@@ -7,12 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarUserComponent implements OnInit {
   user: any;
+  points: any;
 
-  constructor() { }
+  constructor(private http: PointsService, private router: Router) { }
 
   ngOnInit() {
+    this.user = sessionStorage.getItem('user');
+    // console.log(this.user);
+    this.http.post(this.user).subscribe(data => {
+      this.points = data;
+      // console.log(this.points);
+    });
   }
 
-  onUser() {
+  onCS() {
+    this.router.navigate(['/']);
+    sessionStorage.removeItem('user');
   }
 }

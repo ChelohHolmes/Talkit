f40d811260c9d1dc4a6521074bb8a4b5b4fc9ff0
ChelohHolmes <marcelogerstein@gmail.com>
2019-06-12 20:46:21 -0500

    Update audio component

--- src/app/audio/audio.component.ts
+++ src/app/audio/audio.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Dispositivos {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-audio',
   templateUrl: './audio.component.html',
   styleUrls: ['./audio.component.scss']
 })
 export class AudioComponent implements OnInit {
+  selectedE = 'hamanaE-0';
+  selectedS = 'hamanaS-0';
+
+  dispositivosE: Dispositivos[] = [
+    {value: 'hamanaE-0', viewValue: 'Micrófono Hamana'},
+    {value: 'patitoE-1', viewValue: 'Micrófono Patito'}
+  ];
+
+  dispositivosS: Dispositivos[] = [
+    {value: 'hamanaS-0', viewValue: 'Salida Hamana'},
+    {value: 'patitoS-1', viewValue: 'Salida Patito'}
+  ];
 
   constructor() { }
 
